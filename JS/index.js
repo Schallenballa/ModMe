792d479671a27781c358f02da03d1046b5162fec
Zachary Schallenberger <zapps98@gmail.com>
2018-12-10 17:33:43 -0700

    More bug fixes to user accounts on index.html

--- JS/index.js
+++ JS/index.js
@@ -7,7 +7,24 @@ window.onload = function(){
 
 
   console.log(currentUser);
-  
+
+  //Checks to see if the user is logged in
+  if(!currentUser) {
+      console.log("Not Logged In");
+      document.getElementById('authenticated').style.display = 'block';
+      document.getElementById('authenticated2').style.display = 'block';
+  } else {
+      console.log(currentUser + " is logged in");
+      document.getElementById('authenticated').style.display = 'none';
+      document.getElementById('authenticated2').style.display = 'none';
+  }
+}
+netlifyIdentity.onload = function(){
+  const currentUser = netlifyIdentity.currentUser();
+
+
+  console.log(currentUser);
+
   //Checks to see if the user is logged in
   if(!currentUser) {
       console.log("Not Logged In");
